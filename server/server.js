3ef3d4ccf5f17750ebc38d14b1492d72a535a103
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-23 10:10:01 +0545

    add connect/disconnect sockets

--- server/server.js
+++ server/server.js
@@ -1,10 +1,28 @@
-const path = require('path');
+const socketIO = require('socket.io');
 const express = require('express');
+const path = require('path');
+const http = require('http');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// io.on lets us register event listener
+// we listen for specific event and do something when that event happens
+// 'connection' lets us to listen for a new connection meaning client connected the server
+io.on('connection', (socket) => {
+  console.log('new user connected');
+
+  // client disconnect
+  socket.on('disconnect', () => {
+    console.log('client disconnected');
+  });
+});
+
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`server is running on port: ${port}`);
 });
