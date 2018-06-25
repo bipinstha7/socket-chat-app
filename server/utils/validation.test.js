e8adbb11ea2a87e85cf41b6276d5d8b22888d5f6
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-25 14:47:16 +0545

    form validation test

--- server/utils/validation.test.js
+++ server/utils/validation.test.js
@@ -3,6 +3,16 @@ const {isRealString} = require('./validation');
 describe('isRealString', () => {
   it('should reject non-string values', () => {
     let res = isRealString(98);
-    expect(res).
+    expect(res).toBeFalsy();
+  });
+
+  it('should reject string with only spaces', () => {
+    let res = isRealString('     ');
+    expect(res).toBeFalsy();
+  });
+
+  it('should allow string with non-space characters', () => {
+    let res = isRealString('  bipin   ');
+    expect(res).toBeTruthy();
   });
 });
