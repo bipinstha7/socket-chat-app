9c34cded395e965509052231750e0dd0280ebdbd
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-25 11:13:24 +0545

    validation test file

---
+++ server/utils/validation.test.js
@@ -0,0 +1,8 @@
+const {isRealString} = require('./validation');
+
+describe('isRealString', () => {
+  it('should reject non-string values', () => {
+    let res = isRealString(98);
+    expect(res).
+  });
+});
